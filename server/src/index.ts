f12c2b79a35bef348785c68f89a20880d2a73b6a
rodrigo-contreras-sf <rodry2580@gmail.com>
2021-09-27 15:09:09 -0300

    Reforma Index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,30 @@
 import express from 'express';
+import CommonRoutes from './http/routes/common.routes';
+import cors from 'cors';
+import RateRoutes from './http/routes/rates.routes';
 import { log } from 'debug';
-
+import expressWinston from 'express-winston';
+import winston from 'winston';
 const app: express.Application = express();
-
-app.get('/', (_req: express.Request, res: express.Response) => {
-  res.send('Hola');
-})
-
+const loggerOptions: expressWinston.LoggerOptions = {
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+      winston.format.json(),
+      winston.format.prettyPrint(),
+      winston.format.colorize({ all: true })
+  ),
+};
+if (!process.env.DEBUG) {
+    loggerOptions.meta = false; // when not debugging, log requests as one-liners
+}
+app.use(expressWinston.logger(loggerOptions));
+const routes: Array<CommonRoutes> = [];
+app.use(cors());
+app.use(express.json());
+routes.push(new RateRoutes(app));
 app.listen(3000, () => {
+  routes.forEach((route: CommonRoutes) => {
+    log(`Routes configured for ${route.getName()}`);
+  });
   log('Server listening on port 3000');
 });
