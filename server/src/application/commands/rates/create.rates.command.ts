629a3335a3b76c3efeec0c6e9b1874b8d58452da
rodrigo-contreras-sf <rodry2580@gmail.com>
2021-09-28 16:51:46 -0300

    clases rates.command

--- server/src/application/commands/rates/create.rates.command.ts
+++ server/src/application/commands/rates/create.rates.command.ts
@@ -1,11 +1,55 @@
 import { Seniority } from "../../../domain/enums/seniority.enum"
 import { Language } from "../../../domain/enums/language.enum"
+import shortid from "shortid";
 
-export interface CreateRateCommand {
-    technology: number;
-    seniority: Seniority;
-    language: Language;
-    averageSalary: string;
-    grossMargin: string;
-    currency: string;
+class CreateRateCommand {
+    // id con short id ?
+    private id: string;
+    private technology: number;
+    private seniority: Seniority;
+    private language: Language;
+    private averageSalary: string;
+    private grossMargin: string;
+    private currency: string;
+
+    constructor(
+        technology: number,
+        seniority: Seniority,
+        language: Language,
+        averageSalary: string,
+        grossMargin: string,
+        currency: string
+        )
+    {
+        this.id = shortid.generate();
+        this.technology = technology;
+        this.seniority = seniority;
+        this.language = language;
+        this.averageSalary = averageSalary;
+        this.grossMargin = grossMargin;
+        this.currency = currency;
+    }
+    public getId(): string {
+        return this.id;
+    }
+    public getTechnology(): number {
+        return this.technology;
+    }
+    public getSeniority(): Seniority {
+        return this.seniority;
+    }
+    public getLanguage(): Language {
+        return this.language;
+    }
+    public getAverageSalary(): string {
+        return this.averageSalary;
+    }
+    public getGrossMargin(): string {
+        return this.grossMargin;
+    }
+    public getCurrency(): string {
+        return this.currency;
+    }
 }
+
+export default CreateRateCommand
--- server/src/application/commands/rates/delete.rates.command.ts
+++ server/src/application/commands/rates/delete.rates.command.ts
@@ -1,3 +1,13 @@
-export interface DeleteRateCommand {
-    id: number;
+class DeleteRateCommand {
+    private id: string;
+
+    constructor(id: string) {
+        this.id = id;
+    }
+    
+    public getId(): string {
+        return this.id;
+    }
 }
+
+export default DeleteRateCommand
