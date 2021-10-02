8cd0389c93a7f7594d7894485ff79b03f5702801
welschen99 <welschen99@gmail.com>
2021-10-01 21:02:04 -0300

    1/10 mejorado

---
+++ server/src/application/handlers/rates/delete.rates.handler.ts
@@ -0,0 +1,12 @@
+import RatesRepository from "../../../infrastructure/repositories/rate.repository";
+import  DeleteRateCommand  from "../../commands /rates/delete.rates.command";
+class DeleteRateHandler {
+    async execute(command: DeleteRateCommand) {
+        const rates = await RatesRepository.findOneById(command.getId());
+        if (!rates) {
+            throw new Error('Rate not found');
+        }
+        await RatesRepository.deleteById(command.getId());
+    }
+}
+export default new DeleteRateHandler();
